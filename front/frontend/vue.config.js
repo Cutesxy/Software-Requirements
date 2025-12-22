1e1498898f41c6956ee13ef86b4a84ea673315b1
future9541 <153882629+future9541@users.noreply.github.com>
2025-12-23 00:58:46 +0800

    Update vue.config.js

--- front/frontend/vue.config.js
+++ front/frontend/vue.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   devServer: {
     port: 8080,
-    open: true
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5319', // 根据您的测试，后端运行在5319端口
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   },
   css: {
     loaderOptions: {
